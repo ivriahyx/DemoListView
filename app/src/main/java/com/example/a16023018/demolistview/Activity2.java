a00863c1dfc54101c8aa035556aef3a38c92df87
16023018 <16023018@rp.edu.sg>
2018-04-24 12:54:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16023018/demolistview/Activity2.java
@@ -0,0 +1,24 @@
+package com.example.a16023018.demolistview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activities2);
+        // Get the intent so as to get the "things" inside the intent
+        Intent i = getIntent();
+        // Get the String array named "info" we passed in
+        String[] info = i.getStringArrayExtra("info");
+        // Get the TextView object
+        TextView tv1 = (TextView) findViewById(R.id.textView1);
+        // Display the name and age on the TextView
+        tv1.setText(" " + info[0] + info[1]);
+    }
+
+
+}
